54fd37fceb9b856a45a08549a7c4b9852e9efa0c
osf-Jonah <jonah.deleseleuc@osf-commerce.com>
2019-09-29 10:26:28 -0400

    Popular Items update

--- js/navigation.js
+++ js/navigation.js
@@ -2,20 +2,16 @@ if($(window).width() >= 1280){
     $('.d-nav .drop').click (function(){
         $(this).toggleClass('open');   
     });
-    $('.d-nav .drop-first').addClass('.open');
-}
-if($(window).width() <= 1280){
-    $('.d-nav .drop-first').removeClass('.open');
 }
 if ($(window).width() <= 1280){
     $('.m-nav .drop').click (function(){
         $(this).toggleClass('open');   
     });
-    $('footer .drop').click (function(){
-        $(this).toggleClass('open');   
-    });
     $('.m-nav .drop-first').click (function(){
         $(this).toggleClass('open');
+    });
+    $('footer .drop').click (function(){
+        $(this).toggleClass('open');   
     });
 }
 new Date().getFullYear()
